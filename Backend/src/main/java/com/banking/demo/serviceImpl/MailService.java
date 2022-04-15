7dc6612178a8024421e0ecb4427ca3b34ccd99ae
Parmar Dharmendar <dharmendar1123@gmail.com>
2022-04-15 20:36:34 +0530

    Complete project

--- Backend/src/main/java/com/banking/demo/serviceImpl/MailService.java
+++ Backend/src/main/java/com/banking/demo/serviceImpl/MailService.java
@@ -14,13 +14,7 @@ import javax.mail.internet.MimeMessage;
 @Service
 public class MailService {
 
-	static final String FROM="bankingparkar@gmail.com";
-	static final String FROMNAME="Parkar Bank";
-	static final String SMTP_USERNAME="bankingparkar@gmail.com";
-	static final String SMTP_PASSWORD="Qwerty@1234";
-	static final String HOST ="smtp.gmail.com";
-	static final String PORT ="587";
-	static final String SUBJECT="Banking OTP";
+
 
 
 	public boolean sendMail(String email, String oneTimePassword) {
